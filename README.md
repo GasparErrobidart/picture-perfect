e4762f596e3c20785dca88d1dc8294ce93449d89
GasparErrobidart <podmaster@hotmail.com>
2019-05-31 19:44:24 -0300

    V 1.0

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# picture-pergect
+# Picture Perfect
 A tiny code snippet that enhances the picture HTML element.
+Reduce your pages initial load times around 90%, without compromising your users experience.
+Let Picture Perfect calculate the srcset and sizes of your images.
+
+[Live demo](https://jsfiddle.net/8ab0xnef/)
+
+## Features
+- ✔ Lazy loading.
+- ✔ Configurable threshold / viewport offset.
+- ✔ Automatic hand picked sizes.
+- ✔ Automatic srcset.
+- ✔ Automatic srcset with multiple pixel densities.
+- ✔ Specify multiple formats.
+- ✔ Specify custom media rules.
+- ✔ URL string interpolation, enabling performance budget.
+- ✔ Extends your base sizes and srcset configuration.
+- ✔ Art direction, use the native picture features.
+
+## Getting started
+
+Import the snippet into your html file:
+```HTML
+<script src="main.min.js"></script>
+```
+
+Now you can start using the perfect picture:
+```HTML
+<!--
+  Picture Perfect uses <picture> HTML tags to enhance your image delivery.
+  You can provide options as HTML attribute for this tag:
+
+  [data-dynamic-url]
+    !Mandatory
+    ${format} will be replaced dinamically with the current image format
+    ${width} will be replaced dinamically with the current element width
+  [data-densities]
+    Provide a list of pixel density multipliers comma separated.
+    Default : 1
+    e.g.: "1,2,3,4" (this will become 1x, 2x, 3x, 4x the current element width)
+  [data-automatic-sizes]
+    Default: true
+    Set to false to disable
+  [data-automatic-srcset]
+    Default: true
+    Set to false to disable
+  [proximityThreshold]
+    How many pixels before the element gets into the viewport will trigger the lazy loader.
+    Default: window.innerHeight/2
+    e.g.: "300" (pixels)
+-->
+<picture
+  data-dynamic-url="https://placehold.it/${width}x${width}.${format}?text=${width}x${width}+${format}"
+  data-densities="0.5,1,2,3,4"
+  data-automatic-sizes="true"
+>
+
+  <!--
+    Here you can provide native source HTML elements, use media attributes and define srcset,size and type.
+    For each source Picture Perfect will automatically calculate srcset and sizes based on your configuration.
+    This allows you to specify multiple file formats and rules to display them.
+  -->
+  <source srcset="https://placehold.it/50x50.webp?text=50x50+webp" type="image/webp">
+  <source srcset="https://placehold.it/50x50.jpg?text=50x50+jpeg" type="image/jpeg">
+
+  <!--
+    This next <img> tag is your fallback and hast to be present.
+    Add the "make-picture-perfect" attribute to indicate that you want to track this picture/img pair of tags.
+  -->
+  <img make-picture-perfect alt="fallback" src="https://placehold.it/50x50.jpg">
+
+</picture>
+```
+
+## Support for old browsers
+
+You can use [Picture fill](https://scottjehl.github.io/picturefill/) to support the <picture> tag on older browsers.
---
+++ main.js
@@ -0,0 +1,100 @@
+function PicturePerfect(img){
+  // CONFIGURATION
+  var picture             = img.parentElement;
+  var maxWindowSize       = window.innerWidth;
+  var sources             = [];
+
+  for(var i = 0; i < picture.childNodes.length;i++){
+    var node = picture.childNodes[i];
+    if(node.tagName && node.tagName == "SOURCE"){
+      sources.push({
+        dom : node,
+        format : node.getAttribute("type").replace("image/","")
+      })
+    }
+  };
+
+  var proximityThreshold  = picture.getAttribute("data-threshold") || window.innerHeight/2;
+  var automaticSizes      = picture.getAttribute("data-automatic-sizes") == "false" || true;
+  var automaticSrcset     = picture.getAttribute("data-automatic-srcset") == "false" || true;
+  var densities 		      = (picture.getAttribute('data-densities') || ["1"]).split(",").map( function(n) {return parseFloat(n)});
+  var url						      = picture.getAttribute('data-dynamic-url') || new Error("'data-dynamic-url' must be provided.");
+  var originalSrcset			= picture.getAttribute('srcset') || "";
+
+  // GET HAND PICKED ELEMENT SIZE
+  function handPickSizeFor(){
+    return Math.ceil(( img.getBoundingClientRect().width / window.innerWidth) * 100) + "vw";
+  };
+
+  // SET HAND PICKED ELEMENT SIZE
+  function setElementSizes(dom){
+    picture.childNodes.forEach(function(dom){
+      if(dom.tagName && ["SOURCE","IMG"].indexOf(dom.tagName) > -1){
+        dom.setAttribute("sizes",handPickSizeFor());
+      };
+    });
+  };
+
+  // UPDATE SOURCES
+  function updateSources(){
+    // GET THE CURRENT ELEMENT WIDTH
+    var elementWidth 	= Math.ceil(img.getBoundingClientRect().width);
+    sources.forEach(function(source){
+      var format = source.format;
+      // GENERATE THE SRCSET FOR THIS SPECIFIC WIDTH
+      var srcset = densities.map(function(n){
+        var width = Math.ceil(elementWidth * n);
+        return url
+          .replace(/\$\{format\}/gi, format.replace(/jpeg/gi,"jpg") )
+          .replace(/\$\{width\}/gi , width ) +" "+ width +"w";
+      }).join(",");
+      // CONCATE THE ORIGNAL SRCSET IF THERE'S ONE
+      if(originalSrcset) srcset += "," + originalSrcset;
+      source.dom.setAttribute("srcset",srcset);
+    });
+  }
+
+  // ON VIEWPORT RESIZE
+  var onResize = function(){
+    if(automaticSizes) setElementSizes(img);
+    if(maxWindowSize < window.innerWidth){
+      maxWindowSize = window.innerWidth;
+      if(automaticSrcset) updateSources();
+    }
+  }
+
+
+  // ON VIEWPORT PROXIMITY
+  var onProximity = function(){
+    // CALCULATE ELEMENT SIZES ATTRIBUTE
+    if(automaticSizes) setElementSizes();
+    if(automaticSrcset) updateSources();
+    var tmo;
+    window.addEventListener("resize",function(){
+    	clearTimeout(tmo);
+      tmo = setTimeout(onResize,100);
+    });
+  }
+
+
+  // LAZY LOAD / ON SCROLL
+  var onScroll = function(){
+    var rect = img.getBoundingClientRect();
+    if(rect.top - window.innerHeight < proximityThreshold){
+      onProximity();
+      window.removeEventListener("scroll",onScroll);
+    }
+  };
+  window.addEventListener("scroll",onScroll);
+  onScroll();
+
+}
+
+(function(){
+
+  window.addEventListener('load',function(){
+    // WATCH FOR ELEMENTS
+    document.querySelectorAll('picture > img[make-picture-perfect]').forEach(PicturePerfect);
+  });
+
+})();
