f6e180f1d7bd46035719ccf17c63da55d0c1781f
Gaspar Errobidart <gaspar@bytery.com>
2019-07-29 18:06:03 -0300

    wip

--- picture-perfect.copy.js
+++ picture-perfect.copy.js
@@ -3,58 +3,11 @@
 
 function PicturePerfect(img){
   // CONFIGURATION
-  var picture             = img.parentElement;
-  // var maxWindowSize       = window.innerWidth;
-  var sources             = [];
   var bgWrapper           = img.closest('.bg-image-wrapper');
   var container;
 
-  for(var i = 0; i < picture.childNodes.length;i++){
-    var node = picture.childNodes[i];
-    if(node.tagName && node.tagName == "SOURCE"){
-      sources.push({
-        dom : node,
-        format : node.getAttribute("type").replace("image/",""),
-        originalSrcset : node.getAttribute('srcset') || "",
-        originalDataSrcset : node.getAttribute('data-srcset') || ""
-      })
-    }
-  };
-
-
-  var automaticSrcset     = picture.getAttribute("data-automatic-srcset") !== "false" || true;
-  var densities 		      = (picture.getAttribute('data-densities') || "1").split(",").map( function(n) {return parseFloat(n)});
-  var url						      = picture.getAttribute('data-dynamic-url') || "";
   var mimicBackground     = picture.getAttribute("data-mimic-background") !== "false" || true;
 
-
-
-  // UPDATE SOURCES
-  function updateSources(){
-    // GET THE CURRENT ELEMENT WIDTH
-    var elementWidth 	= Math.ceil(img.getBoundingClientRect().width);
-    // THIS IS A SAFEGUARD IN CASE THAT FOR SOME REASON THE ELEMENT GETS A WIDTH OF 0
-    // THAT COULD BREAK SRCSET
-    if(elementWidth <= 0) elementWidth = 100;
-    sources.forEach(function(source){
-      var format = source.format;
-      var srcset = [];
-      // GENERATE THE SRCSET FOR THIS SPECIFIC WIDTH
-      if(url && automaticSrcset ){
-        srcset = densities.map(function(n){
-          var width = Math.ceil(elementWidth * n);
-          return url
-            .replace(/\$\{format\}/gi, format.replace(/jpeg/gi,"jpg") )
-            .replace(/\$\{width\}/gi , width ) +" "+ width +"w";
-        });
-      }
-
-      // CONCATE THE ORIGNAL SRCSET IF THERE'S ONE
-      if(source.originalSrcset) srcset.push(source.originalSrcset);
-      if(source.originalDataSrcset) srcset.push(source.originalDataSrcset);
-      source.dom.setAttribute("srcset",srcset.join(','));
-    });
-  }
 
   function simulateBG(img){
     if(!container) container = img.closest('.mimic-background-container');
