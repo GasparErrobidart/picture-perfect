2ccd193bbf83cb1a7ac40dc8d74ed99c8d28844f
Gaspar Errobidart <gaspar@bytery.com>
2019-07-31 12:55:15 -0300

    Provide minified versions of the scripts

--- minify.js
+++ minify.js
@@ -1,16 +1,24 @@
 const fs    = require('fs');
 const join  = require('path').join;
+const minify = require('minify');
 const argv  =   require('minimist')(process.argv.slice(2));
 
-let apps    = (argv.files || "").split(",");
-let output  = "";
+const path    = argv.path || "./dist";
+let files = fs.readdirSync(join(__dirname,path))
+              .filter(name => /\.js$/.test(name));
 
-apps.forEach((path)=>{
-  output += fs.readFileSync(join(__dirname,path),'utf-8');
-});
+(async()=>{
 
-if (!fs.existsSync('dist')){
-    fs.mkdirSync('dist');
-}
+  for(let i = 0; i < files.length; i++){
+    try{
+      let name = files[i];
+      let location = join(__dirname,path,name);
+      console.log("Minifying:",location);
+      let minified = await minify( location );
+      fs.writeFileSync( location.replace(/\.js$/,'.min.js') , minified);
+    }catch(e){
+      console.error(e);
+    }
+  }
 
-fs.writeFileSync( join(__dirname,'dist',argv.output) ,output);
+})()
